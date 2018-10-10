f83fdf649e886f5645d2bedd621524301c932471
Armastardo <brandon.abr@hotmail.com>
2018-10-10 15:28:29 -0500

    Ya es dinámico, todavía no funciona

--- Prácticas/autoDin.c
+++ Prácticas/autoDin.c
@@ -8,32 +8,55 @@ int main(){
 	printf("Escribe la cadena: ");
 	char * a = readStringInput();
 
-	Vector * current = NULL;
+	Vector * result = NULL;
+	addState(&result, 1);
 
-	Vector * states = NULL;
-	addState(&states, 1);
-	addState(&states, 3);
+	Transitions * transitions = malloc(sizeof(transitions));
+	transitions -> accepted = 0;
+	transitions -> result = result;
+	transitions -> next = NULL;
 
-	States * table = malloc(sizeof(States));
-	table -> input = 1;
-	table -> result = states;
-	table -> next = NULL;
+	State * states = malloc(sizeof(State));
+	states -> value = 0;
+	states -> table = transitions;
+	states -> next = NULL;
 
-	//printf("El valor de entrada es %i\n", table -> input);
+	Automat * automat = malloc(sizeof(Automat));
+	automat -> start = 0;
+	automat -> end = 1;
+	automat -> states = states;
+
+	State * current = automat -> states;
+	while(automat -> start != current -> value){
+		current = current -> next;
+	}
+
+	printf("El estado inicial es q%i\n", current -> value);
+
+	Vector * new = NULL;
 
 	int i = 0;
 	while(a[i]){
+		printf("El estado actual es q%i\n", current -> value);
 		int value = a[i] - '0';
-		if(value == table -> input){
-			printf("Match! - ");
-			addStates(&current, table -> result);
+		printf("El caracter nu'mero %i es %c.\n", i, a[i]);
+		Transitions * aux = current -> table;
+		
+		while(aux){
+
+			printf("== %i vs %i ==\n", aux -> accepted, value);
+			
+			if(value == aux -> accepted){
+				printf("Valor aceptado\n");
+				addStates(&new, aux -> result);
+			}
+			aux = aux -> next;
 		}
 
-		printf("El caracter nu'mero %i es %c.\n", i, a[i]);
 		i++;
 	}
 
-	printVector(current);
+	printVector(new);
 
 	return 0;
 }
--- Prácticas/ndfa.c
+++ Prácticas/ndfa.c
@@ -1,22 +1,31 @@
 #include <stdlib.h>
 #include "vector.c"
 
+//Transiciones de la tabla de estados
 typedef struct transitions{
 	int accepted;
 	Vector * result;
 	struct transitions * next;
 }Transitions;
 
-typedef struct states{
-	int input;
-	Vector * result;
-	struct states * next; 
-}States;
+//Estructura de la tabla de estados
+typedef struct state{
+	int value;
+	Transitions * table;
+	struct state * next; 
+}State;
 
+//Estructura del autómata
 typedef struct automat{
 	int start;
 	int end;
-	States * states;
+	State * states;
 } Automat;
 
 //Si leo X, entonces se vuelve Q y se crea S
+
+/*Electabuzz
+Magmar
+Likitung
+Laptras
+Snorlas*/
